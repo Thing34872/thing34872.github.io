45488a1a1c59c6daa5caa4d25ec3c263d841fc50
Joe Mama <Thing3487@gmail.com>
2023-05-29 19:24:34 +0800

    Rename Minecraft.html to minecraft.html

---
+++ script.js
@@ -0,0 +1,11 @@
+var location = window.location.pathname
+if (location == '/index.html') {
+console.log('Welcome to my website! Please consider staring this project!')
+} else if (location == '/aboutMe.html') {
+  // sus
+  console.log('Know more about me!')
+} else if (location == '/minecraft.html') {
+  console.log('Get Minecraft cheats here!')
+} else if (location == '/media.html') {
+  console.log('My socials.')
+}
